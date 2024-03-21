adc30de019f6fa886a2362de6d71764c9f97b144
jo-codegirl <joanneh@bitheads.com>
2024-03-21 11:57:12 -0600

    adding relaytestapp missing folders

---
+++ RelayTestApp/Scripts/LobbyMember.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class LobbyMember : HBoxContainer
+{
+    // User's name
+    private Label _nameLabel;
+
+    // Indicates that this user is the host / lobby owner when visible
+    private TextureRect _hostIcon;
+
+    public override void _Ready()
+    {
+        _nameLabel = GetNode<Label>("Name");
+        _hostIcon = GetNode<TextureRect>("HostIcon");
+
+        // Hide the host icon when not necessary (i.e. user is NOT the host / lobby owner)
+        _hostIcon.Hide();
+    }
+
+    /// <summary>
+    /// Set the user's name.
+    /// </summary>
+    /// <param name="name">string value of the user's name</param>
+    public void SetName(string name)
+    {
+        _nameLabel.Text = name;
+    }
+
+    public void SetColour(Color colour)
+    {
+        // TODO:  fix the way colour index is used
+        _nameLabel.AddThemeColorOverride("font_color", colour);
+    }
+
+    /// <summary>
+    /// Display the host icon if the user is the host / lobby owner.
+    /// </summary>
+    /// <param name="userIsHost">bool determining whether the user is the host / lobby owner.</param>
+    public void SetHostIcon(bool userIsHost)
+    {
+        _hostIcon.Visible = userIsHost;
+    }
+}
