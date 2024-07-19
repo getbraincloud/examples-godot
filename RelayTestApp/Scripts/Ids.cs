f7e9616fe3ca98e41775bfc4b5e987491f0a18d0
SpencerH <spencerh@bitheads.com>
2024-07-19 15:28:41 -0400

    Move App Secret Away From Main.cs

---
+++ RelayTestApp/Scripts/Ids.cs
@@ -0,0 +1,8 @@
+public static class Ids 
+{
+	// brainCloud data
+	public static string _url = "https://api.braincloudservers.com/dispatcherv2";
+	public static string _appSecret = "00000000-0000-0000-0000-000000000000";
+	public static string _appID = "00000";
+	public static string _version = "1.0.0";
+}
